53dc5ba12c752a1d6b727e2d843ac024bf8ace03
www-castlehi-com <www.castlehi@gmail.com>
2026-05-28 21:39:27 +0900

    chore: initial & booting

---
+++ src/main/java/com/stay/reservation/bookingpayment/BookingPaymentApplication.java
@@ -0,0 +1,13 @@
+package com.stay.reservation.bookingpayment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingPaymentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingPaymentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stay/reservation/bookingpayment/BookingPaymentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stay.reservation.bookingpayment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingPaymentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
